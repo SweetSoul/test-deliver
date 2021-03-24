2c34d3188c1566b56258f7e608fa96f19fd94c6b
Pedro Mendes <hello@pedrofront.dev>
2021-03-24 12:56:12 -0300

    Adicionando layouts e funcionalidades

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Main from "./pages/Main";
+import Dashboard from "./pages/Dashboard";
+import Collaborator from './pages/Collaborator';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/collaborator/:id" component={Collaborator} />
       <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,10 +1,29 @@
 import axios from "axios"
 import { getToken, isAuthenticated } from "./auth";
 const urlApi = "http://5d8b64ad3c0aaf0014342c2a.mockapi.io/api/v1/collaborator"
+const urlApi2 = "https://randomuser.me/api/"
+
 
 export const requestApi = (endpoint, method="GET", params=null, data=null, token=null) => {
     return axios.request({
         baseURL : urlApi,
+        url : endpoint,
+        method: method.toLowerCase(),
+        params : params,
+        data : data,
+        headers : (isAuthenticated()) ? {
+            'Authorization' : 'Bearer ' + getToken()
+        } : (token !== null) ? {'Authorization': 'Bearer ' + token} : { }
+    })
+    .then(response => response.data)
+    .catch(error => Promise.reject(error))
+    .then(data => Promise.resolve(data))
+
+}
+
+export const requestApi2 = (endpoint, method="GET", params=null, data=null, token=null) => {
+    return axios.request({
+        baseURL : urlApi2,
         url : endpoint,
         method: method.toLowerCase(),
         params : params,
